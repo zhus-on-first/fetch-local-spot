24ca6ea761150feec50dd84e053faa1d4f438e64
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-10-19 09:04:40 -0700

    start full stack and init db with empty revision

--- server/app.py
+++ server/app.py
@@ -3,14 +3,14 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 
 # Add your model imports
-from models import db, User, Report, ReportedPhoto, Location, LocationType, LocationFeature, ReportedFeature
+from models import User, Report, ReportedPhoto, Location, LocationType, LocationFeature, ReportedFeature
 
 # Views go here!
 class Index(Resource):
@@ -20,6 +20,6 @@ class Index(Resource):
 api.add_resource(Index, "/")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
 
