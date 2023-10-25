c1ab9e4bdc2563df45ad007fd5d66c50f294e883
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-10-24 22:00:01 -0700

    added new table

--- server/models.py
+++ server/models.py
@@ -15,7 +15,8 @@ class User(db.Model, SerializerMixin):
     # A user has many reports
     reports = db.relationship("Report", back_populates="user")
 
-    # Association Proxy
+    # A user has many reported photos through reports
+    reported_photos = association_proxy("reports", "reported_photos")
 
     # Serialization
     serialize_rules = ("-reports",)
@@ -43,12 +44,6 @@ class Report(db.Model, SerializerMixin):
     # A report has a/belongs to a location
     location = db.relationship("Location", back_populates="reports")
 
-    # Has many features through reported_features: report.features
-    features = association_proxy("reported_features", "location_features.feature")
-
-    # Has many locations through reported_features: report.locations
-    locations = association_proxy("reported_features", "location_features.location")
-
     # Serialization
     serialize_rules = ("-reported_features", "-reported_photos", "-user", "-location")
 
@@ -65,9 +60,6 @@ class ReportedPhoto(db.Model, SerializerMixin):
     # A reported photo has a/belongs to a report
     report = db.relationship("Report", back_populates="reported_photos")
 
-    # Has many features through reported_features
-    features = association_proxy("reported_features", "location_feature.feature")
-
     # Serialization
     serialize_rules = ("-report",)
 
@@ -83,29 +75,26 @@ class Location(db.Model, SerializerMixin):
     phone = db.Column(db.String(15))
     location_type_id = db.Column(db.Integer, db.ForeignKey('location_types.id'))
 
-    # A location has many location features
-    location_features = db.relationship("LocationFeature", back_populates="location")
-
     # A location has many reports
     reports = db.relationship("Report", back_populates="location")
 
     # A location has a/belongs to location_type
     location_type = db.relationship("LocationType", back_populates="locations")
 
-    # Has a location name through location_type
-    location_type_name = association_proxy("location_type", "name")
+    # A location has many location_features
+    location_features = db.relationship("LocationFeature", back_populates="location")
 
-    # Has many features through location_features
-    features = association_proxy("location_features", "feature")
+    # Has a location name through location_types
+    location_type_name = association_proxy("location_types", "name")
 
-    # Has many photos through reported_photos
-    photo_urls = association_proxy("reported_photos", "photo_url")
+    # A location has as many photos through reports
+    location_photos = association_proxy("reports", "reported_photos")
 
     # Serialization
-    serialize_rules = ("-location_features", "-reports", "-location_type")
+    serialize_rules = ("-reports", "-location_type")
 
     def __repr__(self):
-        return f"<Location(id={self.id}: name={self.name}, address={self.address}, phone={self.phone}, type_id={self.type_id})>"
+        return f"<Location(id={self.id}: name={self.name}, address={self.address}, phone={self.phone}, location_type_id={self.location_type_id})>"
 
 class LocationType(db.Model, SerializerMixin): # one of "find a hike", "find a food spot", or "find a ride"
     __tablename__ = "location_types" 
@@ -116,54 +105,58 @@ class LocationType(db.Model, SerializerMixin): # one of "find a hike", "find a f
     # A location_type has many locations
     locations = db.relationship("Location", back_populates="location_type")
 
-    # Association Proxy
-
-
     # Serialize rule
     serialize_rules = ("-locations",)
 
     def __repr__(self):
         return f"<LocationType(id={self.id}: name={self.name})>"
 
+class Feature(db.Model, SerializerMixin):
+    __tablename__ = "features"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+
+    # A feature is referenced by many location_features
+    location_features = db.relationship("LocationFeature", back_populates="feature")
+
+    # A feature is referenced by many reported_features
+    reported_features = db.relationship("ReportedFeature", back_populates="feature")
+
 class LocationFeature(db.Model, SerializerMixin):
     __tablename__ = "location_features"
 
     id = db.Column(db.Integer, primary_key=True)
     location_id = db.Column(db.Integer, db.ForeignKey("locations.id"), nullable=False)
-    feature = db.Column(db.String(20))
+    feature_id = db.Column(db.Integer, db.ForeignKey("features.id"), nullable=False)
 
-    # A location feature has a/belongs to a location
+    # A location_feature has a/belongs to a location
     location = db.relationship("Location", back_populates="location_features")
 
-    # A location feature is referenced by many reported_features
-    reported_features = db.relationship("ReportedFeature", back_populates="location_feature")
-
-    # Has many reports through reported_features: location_feature.reports --all reports associated with a feature
-    reports = association_proxy("reported_features", "report")
+    # A location_feature references a feature
+    feature = db.relationship("Feature", back_populates="location_features")
 
     # Serialization
-    serialize_rules = ("-location", "-reported_features")
+    serialize_rules = ("-location", "-feature")
 
     def __repr__(self):
-        return f"<LocationFeature(id={self.id} location_id={self.location_id}, feature={self.feature})>"
+        return f"<LocationFeature(id={self.id} location_id={self.location_id}, feature={self.feature_id})>"
 
 class ReportedFeature(db.Model, SerializerMixin):
     __tablename__ = "reported_features"
 
     id = db.Column(db.Integer, primary_key=True)
     report_id = db.Column(db.Integer, db.ForeignKey("reports.id"), nullable=False)
-    location_feature_id = db.Column(db.Integer, db.ForeignKey("location_features.id"), nullable=False)
+    feature_id = db.Column(db.Integer, db.ForeignKey("features.id"), nullable=False)
 
     # A reported_feature has a/belongs to a report
     report = db.relationship("Report", back_populates="reported_features")
 
-    # A reported_feature has a reference to a location feature
-    location_feature = db.relationship("LocationFeature", back_populates="reported_features")
-
-    # Association Proxy
+    # A reported_feature references a feature
+    feature = db.relationship("Feature", back_populates="reported_features")
 
     # Serialization
-    serialize_rules = ("-location_feature", "-report")
+    serialize_rules = ("-report", "-feature")
 
     def __repr__(self):
-        return f"<ReportedFeature(id={self.id} report_id={self.report_id} location_feature_id={self.location_feature_id})>"
+        return f"<ReportedFeature(id={self.id} report_id={self.report_id} feature_id={self.feature_id})>"
