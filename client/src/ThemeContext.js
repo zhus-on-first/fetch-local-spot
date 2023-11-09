0da7cd90b00b200d73051c1b08cf37adcc0d4430
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-11-08 16:20:08 -0800

    use localstorage to persist dark/light theme

--- client/src/ThemeContext.js
+++ client/src/ThemeContext.js
@@ -4,9 +4,11 @@ import React, { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
 
   useEffect(() => {
+    localStorage.setItem("theme", theme);
     document.body.setAttribute("data-theme", theme);
   }, [theme]);
 
